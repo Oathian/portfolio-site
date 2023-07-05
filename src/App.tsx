8ec34805aab2435db8552e4b5f032f192ce60fa2
Oathian <aidanmaycock@gmail.com>
2023-07-05 21:58:10 +0100

    skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
@@ -13,38 +10,21 @@ import About from "./components/about/about";
 
 const App = () => {
   const [ isLoading, setIsLoading ] = useState(true);
-  
-  const firebaseConfig = {
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  const storage = getStorage(app);
-  
-  // async function func() {
-  //   const docs = await getDocs(collection(db, "skills"))
-  //   const meme = docs.docs.map(async doc => {
-  //     const logoRef = ref(storage, doc.data().path + ".svg");
-  //     const meme2 = await getDownloadURL(logoRef);
-  //     return meme2;
-  //   })
-  //   return meme;
-  // }
-    
-  // console.log(func())
 
   return ( 
     <div>
       <Header />
-      { !isLoading ? 
-        <LoadingSpinner />
-      :
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>      
-      }
+      <div className="content">
+        { !isLoading ? 
+          <LoadingSpinner />
+        :
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>      
+        }
+      </div>
       <Footer />
     </div>
   )
--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,27 +1,80 @@
 import "./about.scss";
 
+import { DiLaravel } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+import { DiPhp } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
+import { DiGit } from "react-icons/di";
+import { GrGatsbyjs } from "react-icons/gr";
+import { FaShopify } from "react-icons/fa";
+import { FaWordpress } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { GrGraphQl } from "react-icons/gr";
+import { SiLivewire } from "react-icons/si";
+
+const skillsArray = [
+    {
+        "name": "Laravel",
+        "icon": DiLaravel
+    },
+    {
+        "name": "Gatsby",
+        "icon": GrGatsbyjs
+    },
+    {
+        "name": "React",
+        "icon": DiReact
+    },
+    {
+        "name": "Shopify",
+        "icon": FaShopify
+    },
+    {
+        "name": "WordPress",
+        "icon": FaWordpress
+    },
+    {
+        "name": "PHP",
+        "icon": DiPhp
+    },
+    {
+        "name": "JavaScript",
+        "icon": IoLogoJavascript
+    },
+    {
+        "name": "GraphQL",
+        "icon": GrGraphQl
+    },
+    {
+        "name": "MongoDB",
+        "icon": DiMongodb
+    },
+    {
+        "name": "Git",
+        "icon": DiGit
+    },
+    {
+        "name": "Livewire",
+        "icon": SiLivewire
+    },
+];
+
 const About = () => {
-    const skillsArray = [
-        {
-            "title" : "Laravel",
-            "image" : ""
-        },
-        {
-            "title" : "Gatsby",
-            "image" : ""
-        }
-    ];
 
     return (
         <div>
             <h1>My work</h1>
             <h2>Skillset</h2>
-            <div>
+            <div className="about-skill-list">
                 { skillsArray && skillsArray.map((skill) => {
+                    const Icon = skill.icon;
                     return (
-                        <div key={`skills-${skill.title}`}>
-                            <img src={ skill.image } alt={`${skill.title} logo`} />
-                            <p>{ skill.title }</p>
+                        <div 
+                            className="about-skill-card"
+                            key={`skills-${skill.name}`}
+                        >
+                            <Icon className="about-skill-icon" />
+                            <p>{ skill.name }</p>
                         </div>
                     )
                 })}
